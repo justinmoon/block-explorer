c210eaeb8e0b62362dccceb5e4c59cf4e021daec
Justin Moon <mail@justinmoon.com>
2019-06-14 14:35:50 -0500

    python-bitcoinrpc hack to prevent broken pipes

---
+++ explorer.py
@@ -0,0 +1,5 @@
+from rpc import RPC
+
+rpc = RPC()
+
+print(rpc.getblockchaininfo())
---
+++ rpc.py
@@ -0,0 +1,13 @@
+from bitcoinrpc.authproxy import AuthServiceProxy
+from dynaconf import settings
+
+
+class RPC:
+    
+    def __getattr__(self, name):
+        """Hack to establish a new AuthServiceProxy every time this is called"""
+        rpc_uri = f"http://{settings.RPC_USERNAME}:{settings.RPC_PASSWORD}@{settings.RPC_IP}:{settings.RPC_PORT}"
+        print(rpc_uri)
+        rpc = AuthServiceProxy(rpc_uri)
+        return getattr(rpc, name)
+
