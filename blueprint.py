eb455ccdd0245238ec0a97ffdd87f24f0cad2bc1
Justin Moon <mail@justinmoon.com>
2019-06-27 03:55:34 -0500

    Combine RPCWrapper and RPCRouter

--- blueprint.py
+++ blueprint.py
@@ -1,7 +1,7 @@
 from bitcoinrpc.authproxy import JSONRPCException
 from flask import Blueprint, redirect, render_template, request, url_for
-from helpers import (get_block_with_txns, get_last_blocks_threaded,
-                     get_tx_with_inputs, rpc, get_tx_for_address, get_last_blocks)
+from rpc import (get_block_with_txns, get_last_blocks_threaded,
+                 get_tx_with_inputs, rpc, get_tx_for_address, get_last_blocks)
 
 def make_blueprint(name):
     blueprint = Blueprint(name, __name__, static_folder='static')
--- rpc.py
+++ rpc.py
@@ -1,22 +1,24 @@
 import ssl
 import toml
+import threading
+import os
 
 from flask import current_app, request
-
 from bitcoinrpc.authproxy import AuthServiceProxy
 
 
 class RPCWrapper:
     
     template = "{TRANSPORT}://{USERNAME}:{PASSWORD}@{HOST}:{PORT}"
+    settings = toml.load('settings.toml')
     
-    def __init__(self, node, network):
+    def __init__(self, node):
         self.node = node
-        self.network = network
-        self.url = self.template.format(**toml.load('settings.toml')[node][network])
 
-    def proxy(self):
-        return AuthServiceProxy(self.url, context=self.context())
+    def proxy(self, network):
+        settings = self.settings[self.node][network]
+        url = self.template.format(**settings)
+        return AuthServiceProxy(url, context=self.context())
 
     def context(self):
         if self.node == 'btcd':
@@ -29,20 +31,91 @@ class RPCWrapper:
 
     def __getattr__(self, name):
         """Hack to establish a new AuthServiceProxy every time this is called"""
-        return getattr(self.proxy(), name)
-
-
-class RPCRouter:
-
-    def __init__(self, node):
-        self.node = node
-        self.testnet = RPCWrapper(node, 'testnet')
-        self.mainnet = RPCWrapper(node, 'mainnet')
-
-    def __getattr__(self, name):
         if current_app and 'testnet' in request.path:
-            print('calling testnet', name)
-            return getattr(self.testnet, name)
+            network = 'testnet'
         else:
-            print('calling mainnet', name)
-            return getattr(self.mainnet, name)
+            network = 'mainnet'
+        print('calling', network, name)
+        return getattr(self.proxy(network), name)
+
+
+
+rpc = RPCWrapper(os.environ.get('NODE', 'bitcoind'))
+
+
+def get_block(x):
+    if isinstance(x, int):
+        blockhash = rpc.getblockhash(x)
+    else:
+        blockhash = x
+    return rpc.getblock(blockhash)
+
+
+def get_block_with_txns(blockhash):
+    if rpc.node == 'bitcoind':
+        return rpc.getblock(blockhash, 2)
+    else:
+        r = rpc.getblock(blockhash, True, True)
+        r['tx'] = r.pop('rawtx')
+        return r
+
+
+def get_blocks_threaded(height, num_blocks):
+    def get_block_threaded(blocks, i):
+        blockhash = rpc.getblockhash(height - i)
+        block = rpc.getblock(blockhash)
+        blocks[i] = block
+
+    blocks = [None] * num_blocks
+    threads = [None] * num_blocks
+
+    for i in range(len(threads)):
+        threads[i] = threading.Thread(target=get_block_threaded, args=(blocks, i))
+        threads[i].start()
+
+    for thread in threads:
+        thread.join()
+
+    return blocks
+
+
+def get_last_blocks_threaded(num_blocks):
+    height = rpc.getblockchaininfo()["blocks"]
+    return get_blocks_threaded(height, num_blocks)
+
+
+def get_blocks(height, num_blocks):
+    """get `num_blocks` starting from `height`"""
+    blocks = []
+    next_block_hash = rpc.getblockhash(height)
+    for _ in range(num_blocks):
+        block = get_block(next_block_hash)
+        next_block_hash = block["previousblockhash"]
+        blocks.append(block)
+    return blocks
+
+
+def get_last_blocks(num_blocks):
+    height = rpc.getblockchaininfo()["blocks"]
+    return get_blocks(height, num_blocks)
+
+
+def get_tx(tx_id):
+    # btcd demands an integer
+    # TODO: explore this and patch either python-jsonrpc or btcd ...
+    return rpc.getrawtransaction(tx_id, 1)  
+
+
+def get_tx_with_inputs(tx_id):
+    tx = get_tx(tx_id)
+    for i, vin in enumerate(tx["vin"]):
+        # Ignore coinbases
+        if "coinbase" not in vin:
+            t = get_tx(vin["txid"])
+            utxo = t["vout"][vin["vout"]]
+            vin["spending"] = utxo
+    return tx
+
+
+def get_tx_for_address(address):
+    return rpc.searchrawtransactions(address)
